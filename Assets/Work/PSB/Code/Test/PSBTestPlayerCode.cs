ddcb132400e0c7be2fc651ad81d68cef0337cd8c
PSB08 <Parkseongbin08@gmail.com>
2025-10-04 16:20:31 +0900

    feat, fix : PSB player test code

--- Assets/Work/PSB/Code/Test/PSBTestPlayerCode.cs
+++ Assets/Work/PSB/Code/Test/PSBTestPlayerCode.cs
@@ -1,114 +1,47 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.Events;
-using Work.CIW.Code.Player;
+﻿using UnityEngine;
+using Work.CIW.Code;
+using Work.CUH.Chuh007Lib.EventBus;
+using Work.CUH.Code.Commands;
+using Work.CUH.Code.GameEvents;
 
 namespace Work.PSB.Code.Test
 {
-    public class PSBTestPlayerCode : MonoBehaviour, IMovement, IGridObject
+    public class PSBTestPlayerCode : MonoBehaviour
     {
-        [Header("Dependencies - DIP")]
-        [SerializeField] MonoBehaviour gridServiceMono;
-        IGridDataService _gridService;
+        [field: SerializeField] public PlayerInputSO InputSO { get; private set; }
 
-        [Header("Movement")]
-        [SerializeField] float moveTime = 0.15f;
-
-        public Vector3Int CurrentGridPosition { get; private set; }
-        public GameObject GetGameObject() => gameObject;
-
-        bool _isMoving = false;
+        [SerializeField] private MoveCommand moveCommand;
+        
+        private PSBTestPlayerMovement _movement;
 
         private void Awake()
         {
-            if (gridServiceMono is IGridDataService service)
-            {
-                _gridService = service;
-            }
-            else
-            {
-                enabled = false;
-            }
+            _movement = GetComponent<PSBTestPlayerMovement>();
+        }
+        
+        private void OnEnable()
+        {
+            if (InputSO != null)
+                InputSO.OnMovement += HandleMove;
         }
 
-        private void Start()
+        private void OnDisable()
         {
-            CurrentGridPosition = Vector3Int.RoundToInt(transform.position);
-            transform.position = CurrentGridPosition;
-
-            _gridService.SetObjectInitialPosition(this, CurrentGridPosition);
+            if (InputSO != null)
+                InputSO.OnMovement -= HandleMove;
         }
 
-        public void HandleInput(Vector2 input)
+        private void HandleMove(Vector2 dir)
         {
-            if (_isMoving) return;
-
-            Vector3Int dir = GetDirection(input);
-            if (dir == Vector3Int.zero) return;
-
-            Vector3Int nextPos = CurrentGridPosition + dir;
-            
-            Collider[] hits = Physics.OverlapSphere(nextPos, 0.1f);
-            bool blockFound = false;
-
-            foreach (Collider hit in hits)
+            if (_movement == null) return;
+            var command = Instantiate(moveCommand);
+            command.Commandable = _movement;
+            command.Dir = dir;
+            if (command.CanExecute())
             {
-                if (hit.TryGetComponent(out BlockPushTest block)) 
-                {
-                    blockFound = true;
-
-                    if (block.CanMove(dir))
-                    {
-                        StartCoroutine(block.MoveRoutine(dir));
-                    }
-                    return;
-                }
+                Bus<CommandEvent>.Raise(new CommandEvent(command));
+                Bus<TurnUseEvent>.Raise(new TurnUseEvent()); 
             }
- 
-            if (!blockFound)
-            {
-                if (_gridService.CanMoveTo(CurrentGridPosition, dir, out Vector3Int targetPos))
-                {
-                    StartCoroutine(MoveRoutine(targetPos));
-                }
-            }
-        }
-
-        private Vector3Int GetDirection(Vector2 input)
-        {
-            if (input.y > 0.5f) return Vector3Int.forward;
-            if (input.y < -0.5f) return Vector3Int.back;
-
-            if (input.x > 0.5f) return Vector3Int.right;
-            if (input.x < -0.5f) return Vector3Int.left;
-            
-            return Vector3Int.zero;
-        }
-
-        private IEnumerator MoveRoutine(Vector3Int targetPos)
-        {
-            _isMoving = true;
-
-            Vector3 start = transform.position;
-
-            float elapsed = 0f;
-            while (elapsed < moveTime)
-            {
-                transform.position = Vector3.Lerp(start, targetPos, elapsed / moveTime);
-                elapsed += Time.deltaTime;
-                yield return null;
-            }
-
-            transform.position = targetPos;
-            _gridService.UpdateObjectPosition(this, CurrentGridPosition, targetPos);
-            CurrentGridPosition = targetPos;
-
-            _isMoving = false;
-        }
-
-        public GameObject GetObject()
-        {
-            return gameObject;
         }
         
     }
