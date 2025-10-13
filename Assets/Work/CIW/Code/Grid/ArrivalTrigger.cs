487f4852a8f8b66ae35523e2c07dfbc993217104
ggach16 <dlsdnj1234@gmail.com>
2025-10-13 23:47:04 +0900

    fixed : player arrive system

---
+++ Assets/Work/CIW/Code/Grid/ArrivalTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Work.CIW.Code.Player;
+
+namespace Work.CIW.Code.Grid
+{
+    public class ArrivalTrigger : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.GetComponent<Player.Player>() != null)
+            {
+                Debug.Log("도착입니다!");
+            }
+        }
+    }
+}
