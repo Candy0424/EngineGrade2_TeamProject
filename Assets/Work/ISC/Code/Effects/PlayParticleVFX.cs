ed8bb5a9ac667b0ea4696611559077044487abaa
Candy <dltkr0424@gmail.com>
2025-10-14 11:27:26 +0900

    feat : PoolingEffect

---
+++ Assets/Work/ISC/Code/Effects/PlayParticleVFX.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
+
+namespace Work.ISC.Code.Effects
+{
+    public class PlayParticleVFX : MonoBehaviour, IPlayableVFX
+    {
+        [field: SerializeField] public string VfxName { get; private set; }
+        [SerializeField] private bool isOnPosition;
+        [SerializeField] private ParticleSystem particle;
+        
+        public void PlayVfx(Vector3 position)
+        {
+            if (isOnPosition == false)
+                transform.SetPositionAndRotation(position, Quaternion.identity);
+
+            particle.Play(true);
+        }
+
+        public void StopVfx()
+        {
+            particle.Stop(true);
+        }
+
+        private void OnValidate()
+        {
+            if(string.IsNullOrEmpty(VfxName) == false)
+                gameObject.name = VfxName;
+        }
+    }
+}
