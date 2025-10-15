af303bbf024ef249fc0c6e656bd0221f04041047
PSB08 <Parkseongbin08@gmail.com>
2025-10-15 09:18:37 +0900

    feat : Undo turn revert, blood effect

---
+++ Assets/Work/PSB/Code/Commands/TurnConsumeCommandSO.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Work.CUH.Chuh007Lib.EventBus;
+using Work.CUH.Code.Commands;
+using Work.CUH.Code.GameEvents;
+
+namespace Work.PSB.Code.Commands
+{
+    [CreateAssetMenu(fileName = "TurnConsumeCommand", menuName = "SO/Commands/TurnConsume", order = 20)]
+    public class TurnConsumeCommandSO : BaseCommandSO
+    {
+        public override bool CanExecute() => true;
+
+        public override void Execute()
+        {
+            Bus<TurnConsumeOnlyEvent>.Raise(new TurnConsumeOnlyEvent());
+        }
+
+        public override void Undo()
+        {
+            Bus<TurnGetEvent>.Raise(new TurnGetEvent());
+        }
+        
+    }
+}
