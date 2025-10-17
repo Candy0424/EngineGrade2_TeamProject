fa58c2349e8e2220dc9d28ce251a545d5fcad6ef
Candy <dltkr0424@gmail.com>
2025-10-18 02:03:23 +0900

    feat : TurnCountUI

--- Assets/Work/ISC/Code/UI/TurnCountUI.cs
+++ Assets/Work/ISC/Code/UI/TurnCountUI.cs
@@ -3,6 +3,7 @@ using Chuh007Lib.Dependencies;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Work.ISC.Code.Managers;
 
 namespace Work.ISC.Code.UI
@@ -10,8 +11,13 @@ namespace Work.ISC.Code.UI
     public class TurnCountUI : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI turnCountText;
+        [SerializeField] private Image image;
         
         [Inject] private TurnCountManager _turnCountManager;
+        private int _currentGage;
+
+        private readonly int _gageHash = Shader.PropertyToID("_gage");
+        private readonly int _maxGageHash = Shader.PropertyToID("_maxgage");
         
         private void Start()
         {
@@ -20,7 +26,10 @@ namespace Work.ISC.Code.UI
 
         private void Initialize()
         {
-            UpdateText(_turnCountManager.CurrentTurnCount);
+            int curCnt = _turnCountManager.CurrentTurnCount;
+            _currentGage = curCnt;
+            image.material.SetInt(_maxGageHash, curCnt);
+            UpdateText(curCnt);
             
             _turnCountManager.OnTurnChangeEvent += UpdateText;
         }
@@ -32,11 +41,14 @@ namespace Work.ISC.Code.UI
 
         private void UpdateText(int v)
         {
+            DOVirtual.Float(_currentGage, v, 0.2f, x => image.material.SetFloat(_gageHash, x)).SetEase(Ease.Linear);
             Sequence seq = DOTween.Sequence();
             seq.Append(DOVirtual.Float(60f, 100f, 0.2f, x => turnCountText.fontSize = x));
             seq.Append(DOVirtual.Float(100f, 60f, 0.2f, x => turnCountText.fontSize = x));
             seq.Play();
             turnCountText.SetText(v.ToString());
+            
+            _currentGage = v;
         }
     }
 }
