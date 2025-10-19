16a83ea591fdf3b3742d819280c07fca3988af92
ggach16 <dlsdnj1234@gmail.com>
2025-10-19 23:03:52 +0900

    fixed : grid player fsm

---
+++ Assets/Work/CIW/Code/Player/States/PlayerDeadState.cs
@@ -0,0 +1,31 @@
+﻿using Blade.Entities;
+using UnityEngine;
+
+namespace Work.CIW.Code.Player.States
+{
+    public class PlayerDeadState : PlayerState
+    {
+        public PlayerDeadState(Entity entity, int animationHash) : base(entity, animationHash)
+        {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            _player.ChangeState("DEAD");
+            Debug.Log("까악까악 플레이어 사망 플레이어 사망");
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+            
+        }
+    }
+}
