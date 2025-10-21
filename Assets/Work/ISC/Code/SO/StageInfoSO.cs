75013bb2354067bfd3cc2238fa7fc85d6e7800f1
Candy <dltkr0424@gmail.com>
2025-10-21 10:02:26 +0900

    feat : LobbyPanel

---
+++ Assets/Work/ISC/Code/SO/StageInfoSO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Work.ISC.Code.SO
+{
+    [CreateAssetMenu(fileName = "New StageLevelInfo", menuName = "SO/StageLevelInfo", order = 0)]
+    public class StageInfoSO : ScriptableObject
+    {
+        public int level;
+        public Sprite stageImg;
+        public string stageName;
+        
+        [TextArea]
+        public string description;
+    }
+}
