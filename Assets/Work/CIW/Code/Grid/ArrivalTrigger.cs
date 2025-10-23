9a17fd91b5e4873ace92269f295b7358816a9e05
ggach16 <dlsdnj1234@gmail.com>
2025-10-23 10:56:45 +0900

    fixed : player dead state

--- Assets/Work/CIW/Code/Grid/ArrivalTrigger.cs
+++ Assets/Work/CIW/Code/Grid/ArrivalTrigger.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using Work.CIW.Code.Camera;
+using Work.CIW.Code.Player.Event;
+using Work.CUH.Chuh007Lib.EventBus;
 using Work.PSB.Code.Test;
 
 namespace Work.CIW.Code.Grid
@@ -13,14 +15,22 @@ namespace Work.CIW.Code.Grid
 
         // event bus로 변환해서 해야해
         public UnityEvent OnArrival;
+
+        bool _isArrival = false;
         
         private void OnTriggerEnter(Collider other)
         {
+            if (_isArrival) return;
+
             Debug.Log("TriggerEnter");
             if (other.gameObject.GetComponent<PSBTestPlayerCode>() != null)
             {
+                _isArrival = true;
+
                 OnArrival.Invoke();
-                //LobbyScene();
+
+                Bus<GameClearEvent>.Raise(new GameClearEvent());
+
                 Debug.Log("도착입니다!");
             }
         }
