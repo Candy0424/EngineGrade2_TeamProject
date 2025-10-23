30e30a362d5543b1285feab56ddb3f7b1279e2ce
PSB08 <Parkseongbin08@gmail.com>
2025-10-23 16:36:28 +0900

    feat : Dead ui, start timeline fix

--- Assets/Work/CIW/Code/Player/States/PlayerDeadState.cs
+++ Assets/Work/CIW/Code/Player/States/PlayerDeadState.cs
@@ -23,31 +23,11 @@ namespace Work.CIW.Code.Player.States
 
             _player.StopAllCoroutines();
 
+            _player.InkPooling();
+            _fsmHost.OnDeadEvent?.Invoke();
             _fsmHost.enabled = false;
-
-            //_player.ChangeState("DEAD");
-
-            _player.StartCoroutine(DeathSequence());
         }
 
-        public override void Update()
-        {
-            
-        }
-
-        public override void Exit()
-        {
-            base.Exit();
-        }
-
-        private IEnumerator DeathSequence()
-        {
-            yield return _player.InkPooling();
-
-            // 끝났을 때, 책 덮고 로비로 나가야한다.
-            Debug.Log("진짜로 죽음처리 끝남");
-            yield return new WaitForSeconds(0.5f);
-            SceneManager.LoadScene("BookScene");
-        }
+        
     }
 }
