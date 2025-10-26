90781a548f3d029484eb387b17d46ff7e806d81b
PSB08 <Parkseongbin08@gmail.com>
2025-10-26 22:32:20 +0900

    fix : map impulse listener

---
+++ Assets/Work/PSB/Code/Managers/BackMusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ami.BroAudio;
+using UnityEngine;
+
+namespace Work.PSB.Code.Managers
+{
+    public class BackMusicPlayer : MonoBehaviour
+    {
+        [SerializeField] private SoundID backMusic;
+
+        private void Start()
+        {
+            BroAudio.Play(backMusic);    
+        }
+        
+    }
+}
