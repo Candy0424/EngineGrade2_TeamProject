552a20311890c0b8c2c087f65eeb3b1d9fb08394
Candy <dltkr0424@gmail.com>
2025-10-28 23:54:39 +0900

    feat : ResolutionDropBoxUI

--- Assets/Work/ISC/Code/UI/ResolutionDropBoxUI.cs
+++ Assets/Work/ISC/Code/UI/ResolutionDropBoxUI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Work.ISC.Code.UI
@@ -10,29 +11,39 @@ namespace Work.ISC.Code.UI
         [SerializeField] private TMP_Dropdown dropdown;
 
         private List<Resolution> _resolutions;
+        private List<string> _dropdownMenus;
         
         private void Awake()
         {
             _resolutions = new List<Resolution>();
+            _dropdownMenus = new List<string>();
             Initialize();
         }
 
         private void Initialize()
         {
+            dropdown.ClearOptions();
 
             foreach (var resolution in Screen.resolutions)
             {
-                if ((resolution.width == 1280 && resolution.height == 720) || (resolution.width > 1600 && resolution.height > 1000))
+                if ((resolution.width == 1280 && resolution.height == 720) ||
+                    (resolution.width > 1600 && resolution.height > 1000))
+                {
+                    RefreshRate rate = resolution.refreshRateRatio;
+                    uint refreshRate = rate.numerator / rate.denominator;
+                    string str = $"{resolution.width}x{resolution.height} {refreshRate}Hz";
+                    _dropdownMenus.Add(str);
                     _resolutions.Add(resolution);
+                }
             }
             
-            Debug.Log(Screen.resolutions.Length);
-            foreach (var resolution in _resolutions)
-            {
-                RefreshRate rate = resolution.refreshRateRatio;
-                uint refreshRate = rate.numerator / rate.denominator;
-                Debug.Log(resolution.width + " x " + resolution.height + " " + refreshRate + "Hz");
-            }
+            dropdown.AddOptions(_dropdownMenus);
+        }
+
+        public void SetResolution()
+        {
+            Resolution res = _resolutions[dropdown.value];
+            Screen.SetResolution(res.width, res.height, false);
         }
     }
 }
